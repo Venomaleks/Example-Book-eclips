3fafb36ca0deb20a297528c2d04bbcc0eace3b44
Venomaleks <aleks.edholm@gmail.com>
2021-03-09 09:38:32 +0100

    Library extra change

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,23 @@ public class Main {
 		
 		System.out.println(book1.toString());
 		
-		book1.setAuthor("Rafael Silva");
+		/*	book1.setAuthor("Rafael Silva");
 		book1.setYear(2005);
 		book1.setPublisher("DAN THE MAN");
-		
+	*/	
 		System.out.println(book1.toString());
 		
-	/*	Book book3 = new Book("Publisher3", 2012, "SKKKRRRR");
+		Book book3 = new Book("Publisher3", 2012, "SKKKRRRR");
 		Book book4 = new Book("Publisher4", 2012, "YAAAASSS");
 		Book book5 = new Book("Publisher5", 2012, "ABAOOO");
 		Book book6 = new Book("Publisher6", 2012, "SKURR");
 		Book book7 = new Book("Publisher7", 2012, "HAY");
 		Book book8 = new Book("Publisher8", 2012, "BRRAA");
-		*/
+		
 		
 		System.out.println(Book.numberOfBooks);
 
+		// this is a test
 	}
 
 }
